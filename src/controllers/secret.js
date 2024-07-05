39af3951430d2a6dd9b77d083202f7cb7f3e9c5e
Will Baxter <bax.will@googlemail.com>
2024-07-05 17:20:12 +0100

    Fetch secrets with auth

--- src/controllers/secret.js
+++ src/controllers/secret.js
@@ -1,19 +1,23 @@
-const { getSecretDb, postSecretDb } = require('../dal/secret.js')
+const { getSecretDb, postSecretDb } = require("../dal/secret.js");
+const jwt = require('jsonwebtoken')
 
-async function getSecretController (req, res) {
-    const userId = Number(req.params.id)
-    const secrets = await getSecretDb(userId)
-    res.status(200).json({ secrets })
+async function getSecretController(req, res) {
+    const authKey = req.headers.bearer;
+    const userId = Number(req.params.id);
+  try {
+    jwt.verify(authKey, process.env.JWT_TOKEN);
+    const secrets = await getSecretDb(userId);
+    res.status(200).json({ secrets });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-async function postSecretController (req, res) {
-    const userId = Number(req.params.id)
-    const content = req.body.secret
-    const secret = await postSecretDb(userId, content)
-    res.status(201).json({ secret })
+async function postSecretController(req, res) {
+  const userId = Number(req.params.id);
+  const content = req.body.secret;
+  const secret = await postSecretDb(userId, content);
+  res.status(201).json({ secret });
 }
 
-
-
-
-module.exports = { getSecretController, postSecretController }
+module.exports = { getSecretController, postSecretController };
